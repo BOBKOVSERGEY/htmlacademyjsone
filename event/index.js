597ae9478a47d6dca6e6ce34d7e1779923972418
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-04-02 12:28:52 +0300

    Реагирование на нажатии клавиши клавиатуры

--- event/index.js
+++ event/index.js
@@ -13,7 +13,18 @@ cart.addEventListener('click', function (e) {
 
 setupOpen.addEventListener('click', function (e) {
   setup.classList.toggle('hidden'); // удаляем/добавляем класс
-  e.stopPropagation(); // останавливаем всплытие
+  //e.stopPropagation(); // останавливаем всплытие
+
+  // события нажатия на клавиши
+  document.addEventListener('keydown', function (e) {
+
+    //если нажали esc
+    if (e.keyCode === 27) {
+      setup.classList.add('hidden'); // удаляем/добавляем класс
+    }
+
+  })
+
 });
 close.addEventListener('click', function () {
   setup.classList.add('hidden'); // добавляем класс
@@ -23,5 +34,19 @@ document.addEventListener('click', function (e) {
   var elClass = e.target.className;
   var attrHref = e.target.href;
   console.log(elClass);
-  console.log(attrHref);
+  //console.log(attrHref);
+});
+
+// валицация форм
+var userNameInput = document.querySelector('.form__input');
+userNameInput.addEventListener('invalid', function (e) {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-x символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  }
 })
+
+
